10e27f239d16679720516f46de00b5b9155f9a2d
Eshi <michaelndolo17142@gmail.com>
2024-09-04 22:30:24 +0300

    adding date_time_helper.dart

---
+++ lib/datetime/date_time_helper.dart
@@ -0,0 +1,15 @@
+String convertDateTimeToString(DateTime datetime){
+   String year = datetime.year.toString();
+   String month = datetime.month.toString();
+   if(month.length==1){
+     month = '0$month';
+   }
+   String day = datetime.day.toString();
+   if(day.length == 1){
+     month = '0$day';
+   }
+
+   String yyyymmdd = year+month+day;
+
+   return yyyymmdd;
+}
--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class _myAppState extends State<myApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: true,
       home: Homepage(),
     );
   }
